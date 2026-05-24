7d12e0a0b077cbd069b7cc08938b92ffaecbd8aa
AlexForLinux <AlexForLinux@users.noreply.github.com>
2026-05-24 00:10:50 +0000

    deploy: 699b28e7ddb88ed24808cfffcc74ad5a74f06161

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-89572ecf.js',
+                path_to_root + 'searchindex-469e2709.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
